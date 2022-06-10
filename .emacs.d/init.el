cb38bcbba5fb449b0f5c143ec0265c7bb6aaedd9
neon_arch <mustafadhuleb53@gmail.com>
2022-06-10 19:07:11 +0300

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,7 +42,7 @@
 ;; do garbage collection automatically in minor mode 
 (use-package gcmh
   :straight t
-  :custom((setq gcmh-high-cons-threshold 500000000))
+  :custom((setq gcmh-high-cons-threshold 1000000000))
   :init(gcmh-mode 1)
   )
 
@@ -67,7 +67,20 @@
 (auto-save-mode t)
 (setq use-dialog-box nil)
 (setq visible-mode t)
-(global-set-key (kbd "C-x w") 'elfeed)
+
+;; function to connect to erc automatically
+(defun neon/connect-irc ()
+  (interactive)
+  (erc-tls
+      :server "irc.libera.chat"
+      :port 6697
+      :nick "slenderHacker"
+      :password (password-store-get "erc/irc")) ;; using pass to retrieve password
+  ) 
+
+;; keybindings 
+(global-set-key (kbd "C-x w") 'elfeed) ;; for elfeed
+(global-set-key (kbd "C-c c c") 'neon/connect-irc) ;; for erc
 
 ;; excluding following modes from showing line numbers
 (dolist (mode '(org-mode-hook
@@ -308,7 +321,7 @@
 ;; open only single dired buffer
 (use-package dired-single
   :straight t
-  :commands (dired dired-jump)
+  :commands (dired dired-jump find-file)
   )
 
 ;; show icons in dired
@@ -571,6 +584,90 @@
   :straight t
   :init (elfeed-goodies/setup)
   )
+
+;; Erc to chat on irc
+(use-package erc
+  :straight nil
+  :commands erc-tls 
+  :config
+  
+  ;; basic configuration
+  (setq erc-server "irc.libera.chat"
+	erc-nick "slenderHacker"    ;; <---- please make sure to change this
+	erc-user-full-name "Legit User"  ;; <---- And this as well
+	erc-track-shorten-start 8
+	erc-autojoin-channels-alist '(("irc.libera.chat" "#systemcrafters" "#emacs"))
+	erc-kill-buffer-on-part t
+	erc-auto-query 'bury)
+
+  ;; add padding to erc chat to right and left
+  (setq erc-fill-column 120
+	erc-fill-function 'erc-fill-static
+	erc-fill-static-center 20)
+
+  ;; hide distractions in erc
+  (setq erc-track-exclude '("#emacs")
+	erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE" "AWAY")
+	erc-hide-list '("JOIN" "NICK" "QUIT" "MODE" "AWAY" "PART")
+	erc-track-exclude-server-buffer t)
+
+  ;; Only use the selected frame for visibility
+  (setq erc-track-visibility nil)
+
+  ;; highlight pal names, fool names and specific words
+  (setq erc-pals '("#acwd")
+        erc-fools '()
+        erc-keywords '("slenderHacker" "python" "c++" "c" "java" "javascript" "html" "css" "lua" "nodejs" "sqlite" "rust"))
+  
+  ;; show notifications when someone mentions my name
+  ;; (add-to-list 'erc-modules 'notifications)
+
+  ;; enable keybindings to switch between erc buffers
+  (setq erc-track-enable-keybindings t)
+  )
+
+;; highlight people's nick name in different colors to differentiate between them
+(use-package erc-hl-nicks
+  :straight t
+  :after erc
+  :config
+  (add-to-list 'erc-modules 'hl-nicks)
+  )
+
+;; show images in erc chat
+(use-package erc-image
+  :straight t
+  :after erc
+  :config
+  (setq erc-image-inline-rescale 300)
+  (add-to-list 'erc-modules 'image)
+  )
+
+;; show emoji icons in erc chat
+(use-package emojify
+  :straight t
+  :hook (erc-mode . emojify-mode)
+  ;; :commands emojify-mode
+  )
+
+;; password management with pass
+(use-package pass
+  :straight t 
+  )
+
+(use-package auth-source-pass
+  :straight t
+  :after dashboard
+  :config
+  (auth-source-pass-enable)
+  )
+
+(require 'epa-file)
+(epa-file-enable)
+(setq epa-pinentry-mode 'loopback)
+
+;; integrating emacs with pass
+(setq auth-sources '(password-store))
 
 ;;org-configuration
 (defun neon/org-config ()
