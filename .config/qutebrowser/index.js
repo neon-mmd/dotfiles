48e99362f0196ed7e70dffcc1f1db49298726c58
neon_arch <mustafadhuleb53@gmail.com>
2022-08-01 16:14:22 +0300

    update qutebrowser

--- .config/qutebrowser/index.js
+++ .config/qutebrowser/index.js
@@ -3,7 +3,7 @@ function make_link() {
     // if you self host your own search engine then uncomment the following line and put your ip and port
     // and make sure to not to remove search query
     // let search_engine = "http://<your-local-ip>:<the-port-on-which-it-is-hosted-on>/search?q=";
-    let search_engine = "https://fe89-94-29-227-80.ngrok.io/?q="; // ---> change this :)
+    let search_engine = "https://search.disroot.org/?q=";
     let link = "";
     search_bar_text = search_bar_text.value.split(" ");
     for (let i = 0; i < search_bar_text.length; i++) {
