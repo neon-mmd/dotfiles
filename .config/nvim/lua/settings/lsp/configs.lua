fec75aa239b045eb11e52130488fe019ff300c71
neon_arch <mustafadhuleb53@gmail.com>
2022-08-11 19:54:14 +0300

    update config.py nvim

---
+++ .config/nvim/lua/settings/lsp/configs.lua
@@ -0,0 +1,20 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+	return
+end
+
+local lspconfig = require("lspconfig")
+
+local servers = { "pylsp", "sumneko_lua","clangd", "rust_analyzer", "denols", "jdtls", "cssls", "html" }
+
+lsp_installer.setup({
+	ensure_installed = servers,
+})
+
+for _, server in pairs(servers) do
+	local opts = {
+		on_attach = require("settings.lsp.handlers").on_attach,
+		capabilities = require("settings.lsp.handlers").capabilities,
+	}
+	lspconfig[server].setup(opts)
+end
--- .config/nvim/lua/settings/lsp/init.lua
+++ .config/nvim/lua/settings/lsp/init.lua
@@ -1,62 +1,8 @@
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
 end
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
--- required
-local lsp_flags = {
-  debounce_text_changes = 150,
-}
-
-local servers = {'pyright',
-                'sumneko_lua',
-                'rust_analyzer'
-                }
-
--- importing nvim-lsp-installer
-local lsp_installer = require("nvim-lsp-installer")
-
-for _, name in pairs(servers) do
-	local server_is_found, server = lsp_installer.get_server(name)
-	if server_is_found then
-		if not server:is_installed() then
-			print("Installing " .. name)
-			server:install()
-		end
-	end
-end
-
-lsp_installer.on_server_ready(function(server)
-	-- Specify the default options which we'll use to setup all servers
-	local default_opts = {
-		on_attach = on_attach,
-		capabilities = capabilities,
-        flags = lsp_flags,
-	}
-
-	server:setup(default_opts)
-end)
+require("settings.lsp.configs")
+require("settings.lsp.handlers").setup()
+require("settings.lsp.null-ls")
