0426aaab87db75a4ca31a1b0aba4d2642303579a
neon_arch <mustafadhuleb53@gmail.com>
2022-09-06 19:21:19 +0300

    update neovim qutebrowser

--- .config/nvim/lua/settings/keybindings/init.lua
+++ .config/nvim/lua/settings/keybindings/init.lua
@@ -4,41 +4,11 @@ local opts = { noremap = true, silent = true }
 bind("", ";", "<Nop>", opts)
 vim.g.mapleader = ";"
 
--- normal
-bind("n","<leader>t",":NvimTreeToggle<CR>",opts)
-bind("n","<leader>li",":LspInstallInfo<CR>",opts)
-bind("n","<leader>ff",":Telescope find_files<CR>",opts)
-bind("n","<leader>rg",":Telescope live_grep<CR>",opts)
-bind("n","<leader>ld",":Telescope diagnostics<CR>",opts)
-bind("n","<leader>s",":ToggleTerm<CR>",opts)
-bind("n","<leader>fb",":Format<CR>",opts)
-bind("n","<leader>u",":PackerSync<CR>",opts)
-bind("n","<leader>g",":Neogit<CR>",opts)
-
--- Move to previous/next
-bind('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-bind('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next
-bind('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-bind('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto buffer in position...
-bind('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-bind('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-bind('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-bind('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-bind('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-bind('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-bind('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-bind('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-bind('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-bind('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
-bind('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
-bind('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
-bind('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
-bind('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-bind('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-bind('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-bind('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+-- window resizing
+bind("n", "<C-Up>", ":resize -2<CR>", opts)
+bind("n", "<C-Down>", ":resize +2<CR>", opts)
+bind("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+bind("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- insert
 bind("i", "jk", "<ESC>", opts)
--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -8,23 +8,23 @@ config.load_autoconfig(False)
 c.content.autoplay = False
 c.colors.webpage.darkmode.enabled = True
 c.scrolling.smooth = True
-c.search.ignore_case = 'always'
+c.search.ignore_case = "always"
 c.downloads.location.directory = os.path.expanduser("~/Downloads")
 c.auto_save.session = False
 c.tabs.mousewheel_switching = False
 
-mySearchEngine = "https://search.disroot.org/" # --> change this :) 
+mySearchEngine = "https://search.disroot.org/"  # --> change this :)
 searchQuery = mySearchEngine + "/?q={}"
 
 ## search engine list
 c.url.searchengines = {
-    'DEFAULT': searchQuery,
-    'yt': 'https://www.youtube.com/results?search_query={}',
-    'aw': 'https://wiki.archlinux.org/title/{}'
+    "DEFAULT": searchQuery,
+    "yt": "https://www.youtube.com/results?search_query={}",
+    "aw": "https://wiki.archlinux.org/title/{}",
 }
 
 # aesthetics
-c.statusbar.show = 'never'
+c.statusbar.show = "never"
 c.fonts.default_family = "FiraCode Nerd Font Mono"
 
 ## startup page
@@ -33,16 +33,20 @@ c.url.start_pages = [startpage]
 c.url.default_page = mySearchEngine
 
 # privacy
-c.content.webrtc_ip_handling_policy = 'disable-non-proxied-udp'
+c.content.webrtc_ip_handling_policy = "disable-non-proxied-udp"
 c.content.webgl = True
 c.content.javascript.enabled = True
 c.content.images = True
 c.content.geolocation = False
-c.qt.chromium.sandboxing = 'enable-all'
-c.content.cookies.accept = 'no-3rdparty'
+c.qt.chromium.sandboxing = "enable-all"
+c.content.cookies.accept = "no-3rdparty"
 c.content.canvas_reading = False
-c.content.headers.custom = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
-c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/100.0"
+c.content.headers.custom = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+}
+c.content.headers.user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/100.0"
+)
 c.content.headers.accept_language = "en-US,en;q=0.5"
 c.content.headers.do_not_track = True
 # c.content.private_browsing = True
@@ -51,9 +55,11 @@ c.content.headers.do_not_track = True
 # c.content.proxy = 'socks://localhost:9050/'
 
 # adblock
-c.content.blocking.method = 'both'
+c.content.blocking.method = "both"
 c.content.blocking.enabled = True
-c.content.blocking.hosts.lists = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts"]
+c.content.blocking.hosts.lists = [
+    "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts"
+]
 
 # performance
 c.content.dns_prefetch = True
@@ -61,7 +67,7 @@ c.content.notifications.enabled = False
 c.scrolling.smooth = True
 
 # Bindings for normal mode
-config.bind('M', 'hint links spawn mpv {hint-url}')
-config.bind('Z', 'hint links spawn st -e youtube-dl {hint-url}')
-config.bind('t', 'set-cmd-text -s :open -t')
-config.bind('xt', 'config-cycle tabs.show always never')
+config.bind("M", "hint links spawn mpv {hint-url}")
+config.bind("Z", "hint links spawn st -e youtube-dl {hint-url}")
+config.bind("t", "set-cmd-text -s :open -t")
+config.bind("xt", "config-cycle tabs.show always never")
