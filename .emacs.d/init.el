1582923dc22c1b090e1ae54f35f75f897b83d755
neon_arch <mustafadhuleb53@gmail.com>
2022-09-07 18:51:24 +0300

    update emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -109,32 +109,77 @@
   :config
   (general-evil-setup t)
 
-  ;; defining a new keymap prefix
-  (define-prefix-command 'ring-map)
-  (global-set-key (kbd "C-a") 'ring-map)
+  ;; defining a new keymap prefix "Ctrl+a"
+  (global-unset-key "\C-a")
+  (defvar ctl-a-map (make-keymap)
+    "Keymap for local bindings and functions, prefixed by (^A)")
+  (define-key global-map "\C-a" 'Control-A-prefix)
+  (fset 'Control-A-prefix ctl-a-map)
+
+  ;; defining a new keymap prefix "Ctrl+t"
+  (global-unset-key "\C-q")
+  (defvar ctl-q-map (make-keymap)
+    "Keymap for local bindings and functions, prefixed by (^Q)")
+  (define-key global-map "\C-q" 'Control-Q-prefix)
+  (fset 'Control-Q-prefix ctl-q-map)
+
+  ;; defining a new keymap prefix "Ctrl+l"
+  (global-unset-key "\C-l")
+  (defvar ctl-l-map (make-keymap)
+    "Keymap for local bindings and functions, prefixed by (^L)")
+  (define-key global-map "\C-l" 'Control-L-prefix)
+  (fset 'Control-L-prefix ctl-l-map)
+
+  ;; defining a new keymap prefix "Ctrl+f"
+  (global-unset-key "\C-j")
+  (defvar ctl-j-map (make-keymap)
+    "Keymap for local bindings and functions, prefixed by (^J)")
+  (define-key global-map "\C-j" 'Control-J-prefix)
+  (fset 'Control-J-prefix ctl-j-map)
 
   (general-create-definer neon/ctrl-a-keys
     :prefix "C-a"
     )
 
   (neon/ctrl-a-keys
-    "a" 'org-agenda
-    "v" 'vterm-other-window
-    "e" 'elfeed
+    "o" 'org-agenda
+    "f" 'elfeed
     "c" 'neon/connect-irc
-    "f" 'lsp-ui-peek-find-references
+    "b" 'eww
+    "e" 'emojify-insert-emoji
+    "a" 'all-the-icons-insert
+    )
+
+  (general-create-definer neon/ctrl-q-keys
+    :prefix "C-q"
+    )
+  
+  (neon/ctrl-q-keys
+    "v" 'vterm-other-window
     "s" 'treemacs
-    "l" 'consult-imenu
+    )
+
+  (general-create-definer neon/ctrl-l-keys
+    :prefix "C-l"
+    )
+
+  (neon/ctrl-l-keys
+    "l" 'lsp-ui-peek-find-references
+    "d" 'consult-lsp-diagnostics
+    "p" 'check-parens
+    "f" 'format-all-buffer
+    )
+
+  (general-create-definer neon/ctrl-j-keys
+    :prefix "C-j"
+    )
+
+  (neon/ctrl-j-keys
+    "i" 'consult-imenu
     "o" 'consult-outline
     "h" 'consult-org-heading
     "m" 'consult-minor-mode-menu
-    "g" 'consult-ripgrep
-    "b" 'eww
-    "p" 'check-parens
-    "d" 'consult-lsp-diagnostics
-    "E" 'emojify-insert-emoji
-    "A" 'all-the-icons-insert
-    "F" 'format-all-buffer
+    "r" 'Consult-Ripgrep
     )
 
   (general-def 'normal 'dired-mode-map
@@ -829,83 +874,27 @@
   :custom
   (setq format-all-default-formatters
 	'(("Assembly" asmfmt)
-	  ("ATS" atsfmt)
-	  ("Bazel" buildifier)
-	  ("BibTeX" emacs-bibtex)
 	  ("C" clang-format)
-	  ("C#" clang-format)
 	  ("C++" clang-format)
-	  ("Cabal Config" cabal-fmt)
-	  ("Clojure" zprint)
 	  ("CMake" cmake-format)
-	  ("Crystal" crystal)
-	  ("CSS" prettier)
-	  ("Cuda" clang-format)
-	  ("D" dfmt)
-	  ("Dart" dart-format)
-	  ("Dhall" dhall)
-	  ("Dockerfile" dockfmt)
-	  ("Elixir" mix-format)
-	  ("Elm" elm-format)
 	  ("Emacs Lisp" emacs-lisp)
-	  ("Erlang" efmt)
-	  ("F#" fantomas)
 	  ("Fish" beautysh)   ; fish-indent
-	  ("Fortran Free Form" fprettify)
-	  ("GLSL" clang-format)
-	  ("Go" gofmt)
-	  ("GraphQL" prettier)
-	  ("Haskell" brittany)
 	  ("HTML" html-tidy)
-	  ("HTML+ERB" erb-format)
 	  ("Java" clang-format)
 	  ("JavaScript" prettier)
 	  ("JSON" prettier)
 	  ("JSON5" prettier)
-	  ("Jsonnet" jsonnetfmt)
 	  ("JSX" prettier)
-	  ("Kotlin" ktlint)
-	  ("LaTeX" latexindent)
-	  ("Less" prettier)
-	  ("Literate Haskell" brittany)
 	  ("Lua" lua-fmt)
 	  ("Markdown" prettier)
-	  ("Nix" nixpkgs-fmt)
-	  ("Objective-C" clang-format)
-	  ("OCaml" ocp-indent)
-	  ("Perl" perltidy)
-	  ("PHP" prettier)
-	  ("Protocol Buffer" clang-format)
-	  ("PureScript" purty)
 	  ("Python" black)
-	  ("R" styler)
-	  ("Reason" bsrefmt)
-	  ("ReScript" rescript)
-	  ("Ruby" rufo)
 	  ("Rust" rustfmt)
-	  ("Scala" scalafmt)
-	  ("SCSS" prettier)
 	  ("Shell" beautysh)
-	  ("Solidity" prettier)
 	  ("SQL" sqlformat)
-	  ("Svelte" prettier)
-	  ("Swift" swiftformat)
-	  ("Terraform" terraform-fmt)
 	  ("TOML" prettier)
-	  ("TSX" prettier)
-	  ("TypeScript" prettier)
-	  ("V" v-fmt)
-	  ("Verilog" istyle-verilog)
-	  ("Vue" prettier)
 	  ("XML" html-tidy)
 	  ("YAML" prettier)
-	  ("Zig" zig)
-	  ("_Angular" prettier)
-	  ("_Flow" prettier)
-	  ("_Gleam" gleam)
-	  ("_Ledger" ledger-mode)
-	  ("_Nginx" nginxfmt)
-	  ("_Snakemake" snakefmt))
+	  )
 	)
 
   :hook (format-all-mode-hook . format-all-ensure-formatter)
