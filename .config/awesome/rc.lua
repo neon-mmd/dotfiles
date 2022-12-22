58295a80d6828fb54be58ce415171c4f992dfec9
neon_arch <mustafadhuleb53@gmail.com>
2022-12-22 11:14:57 +0300

    fix & add notification, notification sound, update widget

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -15,41 +15,47 @@ require("awful.hotkeys_popup.keys")
 
 --------------Errors------------------------
 if awesome.startup_errors then
-	naughty.notify({
-		preset = naughty.config.presets.critical,
-		title = "Oops, there were errors during startup!",
-		text = awesome.startup_errors,
-	})
+    naughty.notify({
+        preset = naughty.config.presets.critical,
+        title = "Oops, there were errors during startup!",
+        text = awesome.startup_errors,
+    })
 end
 
 do
-	local in_error = false
+    local in_error = false
 
-	awesome.connect_signal("debug::error", function(err)
-		if in_error then
-			return
-		end
+    awesome.connect_signal("debug::error", function(err)
+        if in_error then
+            return
+        end
 
-		in_error = true
+        in_error = true
 
-		naughty.notify({
-			preset = naughty.config.presets.critical,
-			title = "Oops, an error happened!",
-			text = tostring(err),
-		})
+        naughty.notify({
+            preset = naughty.config.presets.critical,
+            title = "Oops, an error happened!",
+            text = tostring(err),
+        })
 
-		in_error = false
-	end)
+        in_error = false
+    end)
 end
 
 -- This function will run once every time Awesome is started
 local function run_once(cmd_arr)
-	for _, cmd in ipairs(cmd_arr) do
-		awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
-	end
+    for _, cmd in ipairs(cmd_arr) do
+        awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
+    end
 end
 
 run_once({ "urxvtd", "unclutter -root" }) -- comma-separated entries
+
+function naughty.config.notify_callback(args)
+    local home = os.getenv("HOME")
+    awful.spawn("mpv --no-video --quiet " .. home .. "/.config/awesome/long-pop.wav")
+    return args
+end
 
 ----------------------------------------themes-----------------------------------
 local terminal = "kitty"
@@ -85,19 +91,19 @@ beautiful.init(string.format("%s/.config/awesome/themes/powerarrow/theme.lua", o
 --  Screen
 -- No borders when rearranging only 1 non-floating or maximized client
 screen.connect_signal("arrange", function(s)
-	local only_one = #s.tiled_clients == 1
-	for _, c in pairs(s.clients) do
-		if only_one and not c.floating or c.maximized or c.fullscreen then
-			c.border_width = 0
-		else
-			c.border_width = beautiful.border_width
-		end
-	end
+    local only_one = #s.tiled_clients == 1
+    for _, c in pairs(s.clients) do
+        if only_one and not c.floating or c.maximized or c.fullscreen then
+            c.border_width = 0
+        else
+            c.border_width = beautiful.border_width
+        end
+    end
 end)
 
 -- Create a wibox for each screen and add it
 awful.screen.connect_for_each_screen(function(s)
-	beautiful.at_screen_connect(s)
+    beautiful.at_screen_connect(s)
 end)
 
 --  Mouse bindings
@@ -110,7 +116,7 @@ clientkeys = keybindings.define_client_keybindings()
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
 for i = 1, 9 do
-	globalkeys = keybindings.define_tag_keybindings(globalkeys, i)
+    globalkeys = keybindings.define_tag_keybindings(globalkeys, i)
 end
 
 clientbuttons = buttons.define_client_buttons()
@@ -125,21 +131,21 @@ awful.rules.rules = rules.program_behaviour(clientkeys, clientbuttons)
 -- Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function(c)
-	-- Set the windows at the slave,
-	-- i.e. put it at the end of others instead of setting it master.
-	-- if not awesome.startup then awful.client.setslave(c) end
+    -- Set the windows at the slave,
+    -- i.e. put it at the end of others instead of setting it master.
+    -- if not awesome.startup then awful.client.setslave(c) end
 
-	if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-		-- Prevent clients from being unreachable after screen count changes.
-		awful.placement.no_offscreen(c)
-	end
+    if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
+        -- Prevent clients from being unreachable after screen count changes.
+        awful.placement.no_offscreen(c)
+    end
 end)
 
 client.connect_signal("focus", function(c)
-	c.border_color = beautiful.border_focus
+    c.border_color = beautiful.border_focus
 end)
 client.connect_signal("unfocus", function(c)
-	c.border_color = beautiful.border_normal
+    c.border_color = beautiful.border_normal
 end)
 
 -- autostart programs
--- .config/awesome/themes/powerarrow/theme.lua
+++ .config/awesome/themes/powerarrow/theme.lua
@@ -50,61 +50,68 @@ theme.taglist_spacing = dpi(24)
 theme.taglist_squares_resize = true
 theme.squares_resize = true
 theme.fg_occupied = colors["4"]
+theme.notification_bg = "#000000"
+theme.notification_fg = "#00ff00"
+theme.notification_border_width = theme.border_width
+theme.notification_border_color = "#aaaaaa"
+theme.notification_opacity = 0.85
+theme.notification_shape = gears.shape.rounded_rect
+theme.notification_icon_size = 25
 
 local markup = lain.util.markup
 local separators = lain.util.separators
 
 -- Battery
 local bat = lain.widget.bat({
-	settings = function()
-		if bat_now.status and bat_now.status ~= "N/A" then
-			if bat_now.ac_status == 1 then
-				if not bat_now.perc and tonumber(bat_now.perc) >= 85 then
-					widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% FULL!!"))
-				else
-					widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% "))
-				end
-				return
-			elseif not bat_now.perc and tonumber(bat_now.perc) >= 85 then
-				widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% FULL!!"))
-			elseif not bat_now.perc and tonumber(bat_now.perc) <= 30 then
-				widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% LOW!!"))
-			else
-				widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% "))
-			end
-		else
-			widget:set_markup()
-		end
-	end,
+    settings = function()
+        if bat_now.status and bat_now.status ~= "N/A" then
+            if bat_now.ac_status == 1 then
+                if not bat_now.perc and tonumber(bat_now.perc) >= 85 then
+                    widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% FULL!!"))
+                else
+                    widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% "))
+                end
+                return
+            elseif not bat_now.perc and tonumber(bat_now.perc) >= 85 then
+                widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% FULL!!"))
+            elseif not bat_now.perc and tonumber(bat_now.perc) <= 30 then
+                widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% LOW!!"))
+            else
+                widget:set_markup(markup.font(theme.font, " " .. bat_now.perc .. "% "))
+            end
+        else
+            widget:set_markup()
+        end
+    end,
 })
 
 --TextClock
 local textclock = wibox.widget({
-	font = theme.font,
-	format = "  %Y-%m-%d %a    %I:%M %p",
-	widget = wibox.widget.textclock,
-	refresh = 1,
+    font = theme.font,
+    format = "  %Y-%m-%d %a    %I:%M %p",
+    widget = wibox.widget.textclock,
+    refresh = 1,
 })
 
 local function os_capture(cmd)
-	local f = assert(io.popen(cmd, "r"))
-	local s = assert(f:read("*a"))
-	f:close()
-	s = string.gsub(s, "^%s+", "")
-	s = string.gsub(s, "%s+$", "")
-	s = string.gsub(s, "[\n\r]+", " ")
-	return s
+    local f = assert(io.popen(cmd, "r"))
+    local s = assert(f:read("*a"))
+    f:close()
+    s = string.gsub(s, "^%s+", "")
+    s = string.gsub(s, "%s+$", "")
+    s = string.gsub(s, "[\n\r]+", " ")
+    return s
 end
 
 local function update_pkg()
-	return string.format("  %s Updates", os_capture('bash -c "pacman -Qu | wc -l"'))
+    return string.format("  %s Updates", os_capture('bash -c "pacman -Qu | wc -l || checkupdates | wc -l"'))
 end
 
 local pacupdates = wibox.widget({
-	font = theme.font,
-	format = update_pkg(),
-	refresh = 20,
-	widget = wibox.widget.textclock,
+    font = theme.font,
+    format = update_pkg(),
+    refresh = 20,
+    widget = wibox.widget.textclock,
 })
 
 -- Separators
@@ -112,121 +119,121 @@ local arrow_left = separators.arrow_left
 local arrow_right = separators.arrow_right
 
 function theme.powerline_rl(cr, width, height)
-	local arrow_depth, offset = height / 2, 0
+    local arrow_depth, offset = height / 2, 0
 
-	-- Avoid going out of the (potential) clip area
-	if arrow_depth < 0 then
-		width = width + 2 * arrow_depth
-		offset = -arrow_depth
-	end
+    -- Avoid going out of the (potential) clip area
+    if arrow_depth < 0 then
+        width = width + 2 * arrow_depth
+        offset = -arrow_depth
+    end
 
-	cr:move_to(offset + arrow_depth, 0)
-	cr:line_to(offset + width, 0)
-	cr:line_to(offset + width - arrow_depth, height / 2)
-	cr:line_to(offset + width, height)
-	cr:line_to(offset + arrow_depth, height)
-	cr:line_to(offset, height / 2)
+    cr:move_to(offset + arrow_depth, 0)
+    cr:line_to(offset + width, 0)
+    cr:line_to(offset + width - arrow_depth, height / 2)
+    cr:line_to(offset + width, height)
+    cr:line_to(offset + arrow_depth, height)
+    cr:line_to(offset, height / 2)
 
-	cr:close_path()
+    cr:close_path()
 end
 
 local function pl(widget, bgcolor, padding)
-	return wibox.container.background(wibox.container.margin(widget, dpi(16), dpi(16)), bgcolor, theme.powerline_rl)
+    return wibox.container.background(wibox.container.margin(widget, dpi(16), dpi(16)), bgcolor, theme.powerline_rl)
 end
 
 function theme.at_screen_connect(s)
-	-- Quake application
-	s.quake = lain.util.quake({ app = awful.util.terminal })
+    -- Quake application
+    s.quake = lain.util.quake({ app = awful.util.terminal })
 
-	-- Tags
-	awful.tag(awful.util.tagnames, s, awful.layout.layouts)
+    -- Tags
+    awful.tag(awful.util.tagnames, s, awful.layout.layouts)
 
-	-- Create a promptbox for each screen
-	s.mypromptbox = awful.widget.prompt()
-	-- Create an imagebox widget which will contains an icon indicating which layout we're using.
-	-- We need one layoutbox per screen.
-	s.mylayoutbox = wibox.container.background(awful.widget.layoutbox(s), colors["5"])
-	s.mylayoutbox:buttons(my_table.join(
-		awful.button({}, 1, function()
-			awful.layout.inc(1)
-		end),
-		awful.button({}, 2, function()
-			awful.layout.set(awful.layout.layouts[1])
-		end),
-		awful.button({}, 3, function()
-			awful.layout.inc(-1)
-		end),
-		awful.button({}, 4, function()
-			awful.layout.inc(1)
-		end),
-		awful.button({}, 5, function()
-			awful.layout.inc(-1)
-		end)
-	))
-	-- Create a taglist widget
-	s.mytaglist = wibox.container.background(
-		wibox.container.margin(
-			awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons),
-			dpi(5),
-			dpi(5)
-		),
-		colors["2"]
-	)
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = wibox.container.background(awful.widget.layoutbox(s), colors["5"])
+    s.mylayoutbox:buttons(my_table.join(
+        awful.button({}, 1, function()
+            awful.layout.inc(1)
+        end),
+        awful.button({}, 2, function()
+            awful.layout.set(awful.layout.layouts[1])
+        end),
+        awful.button({}, 3, function()
+            awful.layout.inc(-1)
+        end),
+        awful.button({}, 4, function()
+            awful.layout.inc(1)
+        end),
+        awful.button({}, 5, function()
+            awful.layout.inc(-1)
+        end)
+    ))
+    -- Create a taglist widget
+    s.mytaglist = wibox.container.background(
+        wibox.container.margin(
+            awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons),
+            dpi(5),
+            dpi(5)
+        ),
+        colors["2"]
+    )
 
-	-- Create a tasklist widget
-	s.mytasklist = wibox.container.margin(
-		awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, awful.util.tasklist_buttons),
-		dpi(3),
-		dpi(3)
-	)
+    -- Create a tasklist widget
+    s.mytasklist = wibox.container.margin(
+        awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, awful.util.tasklist_buttons),
+        dpi(3),
+        dpi(3)
+    )
 
-	-- Create the wibox
-	s.mywibox =
-		awful.wibar({ position = "top", screen = s, height = dpi(22), bg = theme.bg_normal, fg = theme.fg_normal })
+    -- Create the wibox
+    s.mywibox =
+    awful.wibar({ position = "top", screen = s, height = dpi(22), bg = theme.bg_normal, fg = theme.fg_normal })
 
-	-- Add widgets to the wibox
-	s.mywibox:setup({
-		layout = wibox.layout.align.horizontal,
-		{ -- Left widgets
-			layout = wibox.layout.fixed.horizontal,
-			spacing = 0,
-			s.mytaglist,
-			arrow_right(colors["2"], colors["bg"]),
-			s.mypromptbox,
-		},
-		s.mytasklist, -- Middle widget
-		{ -- Right widgets
-			layout = wibox.layout.fixed.horizontal,
-			arrow_left(colors["bg"], colors["3"]),
-			wibox.container.background(
-				wibox.container.margin(wibox.widget.systray(), dpi(3), dpi(3), dpi(3), dpi(3)),
-				colors["3"]
-			),
-			arrow_left(colors["3"], colors["4"]),
-			wibox.container.background(
-				wibox.container.margin(
-					wibox.widget({ baticon, bat.widget, layout = wibox.layout.align.horizontal }),
-					dpi(3),
-					dpi(3)
-				),
-				colors["4"]
-			),
-			arrow_left(colors["4"], colors["2"]),
-			wibox.container.background(
-				wibox.container.margin(
-					wibox.widget({ nil, pacupdates, layout = wibox.layout.align.horizontal }),
-					dpi(3),
-					dpi(3)
-				),
-				colors["2"]
-			),
-			arrow_left(colors["2"], colors["1"]),
-			wibox.container.background(wibox.container.margin(textclock, dpi(4), dpi(8)), colors["1"]),
-			arrow_left(colors["1"], colors["5"]),
-			--]]
-			s.mylayoutbox,
-		},
-	})
+    -- Add widgets to the wibox
+    s.mywibox:setup({
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            spacing = 0,
+            s.mytaglist,
+            arrow_right(colors["2"], colors["bg"]),
+            s.mypromptbox,
+        },
+        s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            arrow_left(colors["bg"], colors["3"]),
+            wibox.container.background(
+                wibox.container.margin(wibox.widget.systray(), dpi(3), dpi(3), dpi(3), dpi(3)),
+                colors["3"]
+            ),
+            arrow_left(colors["3"], colors["4"]),
+            wibox.container.background(
+                wibox.container.margin(
+                    wibox.widget({ baticon, bat.widget, layout = wibox.layout.align.horizontal }),
+                    dpi(3),
+                    dpi(3)
+                ),
+                colors["4"]
+            ),
+            arrow_left(colors["4"], colors["2"]),
+            wibox.container.background(
+                wibox.container.margin(
+                    wibox.widget({ nil, pacupdates, layout = wibox.layout.align.horizontal }),
+                    dpi(3),
+                    dpi(3)
+                ),
+                colors["2"]
+            ),
+            arrow_left(colors["2"], colors["1"]),
+            wibox.container.background(wibox.container.margin(textclock, dpi(4), dpi(8)), colors["1"]),
+            arrow_left(colors["1"], colors["5"]),
+            --]]
+            s.mylayoutbox,
+        },
+    })
 end
 
 return theme
